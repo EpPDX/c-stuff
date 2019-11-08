138196359a1ce2add3c6b07b769372ec8af3fed0
U-BSD\348484 <348484@SHS-10DEC3222.beaverton.k12.or.us>
2019-11-08 11:52:20 -0800

    movies

---
+++ classes/movies.cpp
@@ -0,0 +1,53 @@
+//inclusions
+
+#include <iostream>
+#include <string.h>
+#include "media.h"
+#include "movies.h"
+
+using namespace std;
+
+
+
+Movies::Movies()
+{
+
+}
+
+
+char* newTitle, int newYear, char* newDirector, char* newDuration, float newRating
+{
+  director = newDirector;
+  duration = newDuration;
+  rating = newRating;
+}
+
+
+
+
+
+//movie database, gets info
+int Movies::getYear()
+{
+  return year;
+}
+char* Movies::getTitle()
+{
+  return title;
+  }
+char* Movies::getDirector()
+{
+  return director;
+}
+char* Movies::getDuration()
+{
+  return duration;
+}
+float Movies::getRating()
+{
+  return rating;
+  /
+int Movies::getType()
+{
+  return 0;
+}
